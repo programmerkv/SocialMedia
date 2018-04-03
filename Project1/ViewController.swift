8302e840078cea510b3508fb55a676e91fd86f26
Khoa Vo <programmer.kv@gmail.com>
2018-04-03 16:37:55 +0700

    Wrap up

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Storm Viewer"
         // Do any additional setup after loading the view, typically from a nib.
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
@@ -35,6 +37,13 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 
     override func didReceiveMemoryWarning() {
